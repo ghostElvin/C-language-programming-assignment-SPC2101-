a494702cb090d9f9946354f44abb7d4cfb46fff3
Elvin <machariaelvin1@gmail.com>
2025-09-20 21:58:28 +0300

    Add files via upload

---
+++ C language units for this semester.c
@@ -0,0 +1,23 @@
+// Simple C program(Hello World)single line comment
+/*
+Name:Elvin
+Reg no:26559
+Date:12 sep 
+Description:Program to display all units for this semester
+*/
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("This semester units are:\n");
+    printf("Computer Architecture\n");
+    printf("Communication skills\n");
+    printf("Physics\n");
+    printf("Analogue Electronics\n");
+    printf("Fundamentals of ict\n");
+    printf("Introduction to programming\n");
+    printf("Mathematics for science\n");
+    printf("Discrete mathematics");
+    return 0;
+}
