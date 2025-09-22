0905a68e9e984f399bd890228d36e2149caa7a39
Elvin <machariaelvin1@gmail.com>
2025-09-22 10:15:33 +0300

    Add files via upload

---
+++ Loan eligibility.c
@@ -0,0 +1,30 @@
+/*
+Name:Elvin Macharia
+Reg no:26559
+Description:C program to check eligibility of loan
+*/
+
+#include <stdio.h>
+
+int main(){
+    int age;
+    float annual_income;
+    
+    //prompt customer to enter age
+    printf("Enter your age\n");
+    scanf("%d", &age);
+    
+    //prompt customer to enter annual_income
+    printf("Enter your annual_income\n");
+    scanf("%f", &annual_income);
+    
+    //check eligibility
+    if (age>=21 && annual_income>=21000){
+        printf("Congratulations you qualify for a loan\n");
+    }
+    else
+    {
+        printf("Unfortunately,we are unable to offer you a loan at this time\n");
+    }
+    return 0;
+}
---
+++ volume and surface area of cylinder.c
@@ -0,0 +1,32 @@
+/*
+Name:Elvin Macharia
+Reg no:26559
+Description:C program to find the volume and surface area of a cylinder
+volume = pi*r^2*h
+surface area = 2*pi*r^2 + 2*pi*r*h
+*/
+
+#include <stdio.h>
+#define pi 3.1428
+
+int main(){
+    //variable declaration
+    float r,h,volume,surface_area;
+    
+    //prompt user to enter r
+    printf("Enter the radius of the cylinder");
+    scanf("%f", &r);
+    
+    //prompt user to enter h
+    printf("Enter the height of the cylinder\n");
+    scanf("%f", &h);
+    
+    //calculate volume and surface_area
+    volume = pi*r*r*h;
+    surface_area = (2*pi*r*r) + (2*pi*r*h);
+    
+    //output the result
+     printf("Volume of the cylinder: %.2f\n", volume);
+    printf("Surface area of the cylinder: %.2f\n", surface_area);
+    
+}
