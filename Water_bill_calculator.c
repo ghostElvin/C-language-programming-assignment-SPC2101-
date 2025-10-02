74b30ba086fbcf44e5fe9c4e2f1ecca10c175e1a
Elvin <machariaelvin1@gmail.com>
2025-10-02 20:53:21 +0300

    Update and rename Water bill calculator.c to Water_bill_calculator.c

--- Water_bill_calculator.c
+++ Water_bill_calculator.c
@@ -15,17 +15,18 @@ int main() {
 
     //calculate the bill
     if (units <= 30) {
-        Bill = Units * 20.0;
-    } else if (Units <= 60) {
-        Bill = Units * 25.0;
+        bill = units * 20.0;
+    } else if (units <= 60) {
+        bill = units * 25.0;
     } else {
-        Bill = Units * 30.0;
+        bill = units * 30.0;
     }
 
     //display the total bill in decimal
     printf("Total water bill is KES %.2f\n", bill);
     return 0;
 }
+
 
 
 
