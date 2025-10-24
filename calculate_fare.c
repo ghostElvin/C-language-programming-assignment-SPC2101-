2e1daca9702b6d01b67bc4a241bfb2471cb8e6a7
Elvin <machariaelvin1@gmail.com>
2025-10-24 08:00:34 +0300

    Add files via upload

---
+++ calculate_fare.c
@@ -0,0 +1,34 @@
+/*
+Name:Kagwiria Elvin Macharia
+Reg:CT101/G/26559/25
+Description:Fare calculator
+*/
+
+#include <stdio.h>
+
+//Function Prototype
+float calculate_fare(float distance, float rate);
+
+int main(){
+	float rate=50.0;
+	float distance,result;
+	
+	//prompt the user to enter the distance covered
+	printf("Enter the distance covered in kilometers:");
+	scanf("%f",&distance);
+	
+	//function call
+	result=calculate_fare(distance,rate);
+	
+	//output
+	printf("The Total_fare is:%.2f\n",result);
+	
+	return 0;
+}
+//Function definition
+float calculate_fare(float distance,float rate){
+	float Total_fare;
+	Total_fare=distance * rate;
+	
+	return Total_fare;
+}
